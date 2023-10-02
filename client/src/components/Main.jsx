5f6ce50eff148e8f9f00cd1c1b624a4d5c24a235
liaozhu <liao-zhu@uiowa.edu>
2023-10-02 01:56:38 -0500

    Revert "prepare for beta"
    This reverts commit 9dff10091da311e0522ae5dd06902a7bab686df2.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -63,7 +63,7 @@ export function Main() {
     }); 
     const res = await response.json();
     // addMessage([{...nxtValue}, {"type": "AI", "plans": res.plans, "text": "", "startText": "Here is a revised set of courses", "endText": "Does this meet your expectations better?" }], scrollToBottom);
-    addMessage([{...nxtValue},{"type": "human", "text": "We received your question. Thank you kind beta tester!"}], scrollToBottom);
+    addMessage([{...nxtValue},{"type": "human", "text": "We got a response"}], scrollToBottom);
   }
 
   const handleFileUpload = async (e) => {
@@ -114,14 +114,13 @@ export function Main() {
       <div className="py-10 h-[90%]">
       <header>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">What are you wondering?</h1>  
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Have any questions?</h1>  
         </div>
       </header>
       <main className='h-full'>
           <div className="mx-auto max-w-7xl h-full">
             <div className="w-full h-[70%] flex flex-col items-center justify-center">
-              {/*file={file} addFile={handleFileUpload}*/}
-              <CommentForm sendNextQuestion={sendNextQuestion} />
+              <CommentForm sendNextQuestion={sendNextQuestion} file={file} addFile={handleFileUpload}/>
             </div>
           </div>
       </main>
@@ -141,7 +140,7 @@ export function Main() {
           <div className="w-full h-[70%] flex flex-col items-center justify-center">
             <CommentForm 
             sendNextQuestion={setCanvasToken} 
-            placeholder={"Enter your first name"}
+            placeholder={"What is you ICON access token"}
             buttonText={"Submit"}
             />
           </div>
@@ -164,7 +163,7 @@ function CommentForm({sendNextQuestion, placeholder, file, addFile}) {
               value={nextQuestion}
               onChange={(e) => setNextQuestion(e.target.value)}
               className="block w-full resize-none border-0 border-b border-transparent p-0 pb-2 text-gray-900 placeholder:text-gray-400 focus:border-iowaYellow-600 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder={placeholder ? placeholder : `e.g. "what should I study for my networks exam?"`}
+              placeholder={placeholder ? placeholder : "Ask us anything about UIowa..."}
             ></textarea>
           </div>          
           <div className={`flex ${file ? 'justify-between align-center' : 'justify-end'} pt-2`}>
@@ -192,7 +191,7 @@ function CommentForm({sendNextQuestion, placeholder, file, addFile}) {
                 setNextQuestion('');
               }}
             >
-              Submit
+              Sumbit
             </button>
           </div>
         </div>
--- routes.js
+++ routes.js
@@ -12,6 +12,7 @@ const Proompter = require("./proompter");
 const dataProvider = require("./dataProvider");
 
 Routes.post("/home", async (req, res) => {
+    res.json("newuser");
     const { canvasToken } = req.body;
     let existingUser = await user.findOne({ canvasToken });
     if (existingUser) {
@@ -34,15 +35,11 @@ postCanvasData = async(newUser, canvasToken) => {
 Routes.post('/upload', async (req, res) => {
     try {
         const { canvasToken, files } = req.body;
-        // files doesn't seem right
-        for (const file of files) {
-            // save file to uploads directory
-            console.log(file);
-            const fileNameNoDot = file.name.split('.')[0];
-            const filePath = path.join(__dirname, 'userFiles', fileNameNoDot + file.md5 + '.pdf');
-            await file.mv(filePath);
-        }
-            
+        // save file to uploads directory
+        // const fileNameNoDot = file.name.split('.')[0];
+        // const filePath = path.join(__dirname, 'uploads', fileNameNoDot + file.md5 + '.pdf');
+        // await file.mv(filePath);
+    
         // const agent = new Agent(filePath, '', []);
         // const plans = await agent.ready();
         let foundUser = await user.findOne({ canvasToken });
@@ -56,42 +53,32 @@ Routes.post('/upload', async (req, res) => {
 
 Routes.post('/answer', async (req, res) => {
     const { canvasToken, prompt } = req.body;
-
     console.log('we are hitting');
     console.log(canvasToken);
     console.log(prompt);
 
+    // find K most relevant files from  user.personalData, user.canvasData, UIOWAData, combine corresponding vectors, query
+    const kMostRelevant = getTopKRelevant(prompt, canvasToken, k);
+
+    downloadPDFs(kMostRelevant);
+
+    const documents = await new SimpleDirectoryReader().loadData({directoryPath: "./data"});
+    console.log(documents);
+
+    const index = await VectorStoreIndex.fromDocuments(documents);
+
+    const queryEngine = index.asQueryEngine();
+    const response = await queryEngine.query(
+        prompt,
+    );
+
+    console.log(response.toString());    
+
     const foundUser = await user.findOne({ canvasToken });
-    foundUser.questions.push(prompt);
+    foundUser.questions.push([prompt, response]);
     await foundUser.save();
 
     res.json(foundUser);
-
-    // console.log('we are hitting');
-    // console.log(canvasToken);
-    // console.log(prompt);
-
-    // // find K most relevant files from  user.personalData, user.canvasData, UIOWAData, combine corresponding vectors, query
-    // const kMostRelevant = getTopKRelevant(prompt, canvasToken, k);
-
-    // downloadPDFs(kMostRelevant);
-
-    // const documents = await new SimpleDirectoryReader().loadData({directoryPath: "./data"});
-    // console.log(documents);
-
-    // const index = await VectorStoreIndex.fromDocuments(documents);
-
-    // const queryEngine = index.asQueryEngine();
-    // const response = await queryEngine.query(
-    //     prompt,
-    // );
-
-    // console.log(response.toString());    
-
-    // const foundUser = await user.findOne({ canvasToken });
-    // foundUser.questions.push([prompt, response]);
-    // await foundUser.save();
-
 });
 
 getTopKRelevant = async (query, canvasToken, k) => {
