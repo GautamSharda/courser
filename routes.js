3c7d722ab1abc68c9485e24855f851b7061184f2
Ilya Tataurov <ilyat@wireless-nat-inside.uiowa.edu>
2023-10-02 19:20:22 -0500

    chicken

--- routes.js
+++ routes.js
@@ -5,11 +5,14 @@ const express = require("express");
 const Routes = express.Router();
 const user = require("./models/user");
 const fs = require("fs/promises");
-const { Document, VectorStoreIndex, SimpleDirectoryReader } = require("llamaindex");
+const { Document, VectorStoreIndex, SummaryIndex, serviceContextFromDefaults, OpenAI } = require("llamaindex");
 const Canvas = require("./classes/Canvas");
 const { Configuration, OpenAIApi } = require("openai");
 const Proompter = require("./proompter");
 const dataProvider = require("./dataProvider");
+const axios = require('axios');
+const pdf = require('pdf-parse');
+const { MongoClient } = require('mongodb');
 
 Routes.post("/home", async (req, res) => {
     const { canvasToken } = req.body;
@@ -18,18 +21,179 @@ Routes.post("/home", async (req, res) => {
         res.json(existingUser);
     } else {
         let newUser = await user.create({ canvasToken });
-        postCanvasData(newUser, canvasToken);
+        // postCanvasData(newUser, canvasToken);
         res.json(newUser);
     }
 });
+
+
+/** Helper function
+ * 
+ * @param {*} fileUrl this is the URL of the file on the web
+ * @param {*} metadata this is the metadata of the file
+ * @returns a promise that resolves to an array of [summary, rawText]
+ */
+async function processFile(fileUrl, metadata) {
+    const summaryPrompt = "Summarize the contents of this document in 3 sentences. Classify it as lecture, practice test, project, syllabus, etc. Be consise and without filler words."
+    // Download the PDF
+    let startTime = Date.now();
+    let axiosResponse = await axios({
+        url: fileUrl,
+        method: 'GET',
+        responseType: 'arraybuffer',  // Important
+    });
+    let endTime = Date.now();
+    // console.log("Requesting file took " + (endTime - startTime) + " milliseconds");
+
+    // Create Document object 
+    startTime = Date.now();
+    let data = await pdf(axiosResponse.data); // I hate this shit it prints warnings every time
+    endTime = Date.now();
+    // console.log("Parsing PDF took " + (endTime - startTime) + " milliseconds");
+    let document = new Document({ text: data.text, metadata: metadata });
+
+    // Specify LLM model
+    const serviceContext = serviceContextFromDefaults({
+        llm: new OpenAI({ model: "gpt-3.5-turbo", temperature: 0 }),
+    });
+    
+    // Indexing 
+    startTime = Date.now();
+    const index = await SummaryIndex.fromDocuments([document], {serviceContext}); // LlamaIndex embedding
+    // let index = await fetchEmbedding(document.text); // Openai embedding
+    endTime = Date.now();
+    // console.log("Indexing took " + (endTime - startTime) + " milliseconds");
+
+    // Query the index
+    startTime = Date.now();
+    const queryEngine = index.asQueryEngine();
+    const response = await queryEngine.query(
+        summaryPrompt,
+    );
+    endTime = Date.now();
+    // console.log("Query took " + (endTime - startTime) + " milliseconds");
+
+    return [response.toString(), data.text];
+};
+
+/** Helper function
+ * 
+ * @param {*} filesBatch this is an array of files
+ * @param {*} course this is the course object
+ * @returns a promise that resolves to an array of files enriched with summary and raw text
+ */
+async function processFilesBatch(filesBatch, course) {
+    return Promise.all(filesBatch.map(async file => {
+        if (file['content-type'] == "application/pdf") { // Only process PDFs
+            file.course_code = course.course_code;
+            file.course_name = course.name;
+
+            try {
+                const [summary, rawText] = await processFile(file.url, { fileName: file.display_name, created_at: file.created_at });
+                if (summary.length > 0 && rawText.length > 0) {
+                    console.log(file.filename + " summary and raw text ✅")
+                    file.summary = summary;
+                    file.rawText = rawText;
+                } else {
+                    console.log(file.filename + " summary and raw text ❌")
+                }
+            } catch (error) {
+                console.log("❌Error❌ with processing file " + file.filename);
+            }
+        }
+        return file;
+    }));
+}
 
 /** 
  * TODO: Pull all files from Canvas, construct the File object, put it in DB under the newUser 
  * Owner: Ilya 
  */
-postCanvasData = async (newUser, canvasToken) => {
-    const summaryPrompt = "Summarize the contents of this document in 3 sentences. Classify it as lecture, practice test, project, syllabus, etc. Be consise and without filler words."
+postCanvasData = async (canvasToken) => {
+    const mongoClient = new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    
+    const currentTerm = "Fall23" // This is the term that we're currently in, and the only one we want to pull files from, format: Fall23, Fall24, Spr23, Spr24
 
+    let startTime = Date.now();
+    // Connect to MongoDB
+    await mongoClient.connect()
+    const db = mongoClient.db('test');
+    const users = db.collection('users');
+
+    // Step 1: Pull classes from Canvas API
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${canvasToken}`);
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    const classDataResponse = await fetch("https://canvas.instructure.com/api/v1/courses?per_page=1000", requestOptions);
+    let jsonStr = await classDataResponse.text();
+    const modifiedStr = jsonStr.replace(/"id":(\d+)/g, '"id":"$1"');
+
+    let classJson = JSON.parse(modifiedStr); 
+
+    if (classDataResponse.status != 200){
+        console.log(`Canvas enrollment data call ❌ ${classDataResponse.status}-${classDataResponse.statusText}`);
+        return;
+    }
+    else{
+        console.log(`Canvas enrollment data call: 200 ✅`);
+    }
+    
+    // Push classJson to user's DB because we're data collection sluts
+    let mongoPushRes = await users.updateOne(
+        { canvasToken: canvasToken },
+        { $set: { classData: classJson } }
+    );
+    console.log(`Result from enrollment data push:`)
+    console.log(mongoPushRes);
+
+    // Iteratively request files for all classes
+    for(let i=0; i<classJson.length; i++){
+        try {
+            if(classJson[i].course_code.includes(currentTerm)){ // Important to catch only current classes, not past ones
+                // Make get request
+                let filesUrl = `https://canvas.instructure.com/api/v1/courses/${classJson[i].id}/files`
+                let fileDataResponse = await fetch(filesUrl, requestOptions);
+                let filesRes = JSON.parse(await fileDataResponse.text());
+                if (fileDataResponse.status != 200){
+                    continue;
+                } else{
+                    console.log(`✅ Canvas API call successful: 200-OK`);
+                }
+                console.log("Pulling content for " + classJson[i].course_code);
+
+                // Enrich our metadata with summary and raw text 
+                // MULTI THREADING
+                const BATCH_SIZE = 5;
+                let enrichedFiles = [];
+                for (let j = 0; j < filesRes.length; j += BATCH_SIZE) {
+                    const filesBatch = filesRes.slice(j, j + BATCH_SIZE);
+                    await processFilesBatch(filesBatch, classJson[i]);
+                    enrichedFiles = enrichedFiles.concat(filesBatch);
+                }
+
+                // Push file metadata to DB
+                let mongoPushRes = await users.updateOne(
+                    { canvasToken: canvasToken }, 
+                    { $set: { ['files.' + classJson[i].id]: enrichedFiles } }
+                );
+                console.log(`Result from fileData push for ${classJson[i].course_code}:`)
+                console.log(mongoPushRes);
+            }
+        } catch (error) {
+            // Some classes don't have files, so we catch the error and continue
+            console.log("Error with pulling files for class" + classJson[i].id + `[${classJson[i].course_code}]`)
+        }
+    }
+    let endTime = Date.now();
+    console.log("Total time: " + (endTime - startTime) + " milliseconds");
     return;
 }
 
