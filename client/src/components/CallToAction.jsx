7b547ec8916009058a3ead41429b5b183eedc3d3
patfoster <patrick-foster@uiowa.edu>
2023-10-11 17:39:36 -0500

    homepage

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -21,7 +21,7 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Get started today
+            Beta Test Courser
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
             It’s time to take control of your books. Buy our software so you can
