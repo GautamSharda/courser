26cd653670bc25af0ecfdc21aa052bb5345a3da1
Gautam <gautamsharda001@gmail.com>
2023-10-16 04:29:28 -0500

    minor copy change

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,7 +4,7 @@ export function Loader() {
     <div className="full-screen-loader">
       <div className="loader-content">
         <span className="loader"></span>
-        <h1 className="mt-10 font-medium ml-10 text-center text-2xl md:text-3xl">Preparing Courser. This may take a moment. . .</h1> 
+        <h1 className="mt-10 font-medium ml-10 text-center text-2xl md:text-3xl">Courser is getting your Canvas files. This will take a few minutes. . .</h1> 
       </div>
     </div>
   );
